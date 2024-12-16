aac6510163d6854e82589a0ad2ff565bbc35d241
meryem-boudri <meryem-boudri@github.com>
2024-12-16 18:33:53 +0100

    fc

--- conference-service/src/main/java/ma/enset/conferenceservice/repository/ConferenceRepository.java
+++ conference-service/src/main/java/ma/enset/conferenceservice/repository/ConferenceRepository.java
@@ -1,7 +1,6 @@
 package ma.enset.conferenceservice.repository;
 
 import ma.enset.conferenceservice.entities.Conference;
-import ma.enset.keynoteservice.entities.Keynote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
