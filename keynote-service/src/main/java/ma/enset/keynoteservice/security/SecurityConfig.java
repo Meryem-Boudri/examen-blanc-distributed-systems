d5b3c838b6ba0c75511e1b62443d7625507fbf75
meryem-boudri <meryem-boudri@github.com>
2024-12-16 18:33:16 +0100

    fc

---
+++ keynote-service/src/main/java/ma/enset/keynoteservice/security/SecurityConfig.java
@@ -0,0 +1,55 @@
+package ma.enset.keynoteservice.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+    private JwtAuthConverter jwtAuthConverter;
+    public SecurityConfig(JwtAuthConverter jwtAuthConverter) {
+        this.jwtAuthConverter = jwtAuthConverter;
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(Customizer.withDefaults())
+                .sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf -> csrf.disable())
+                .headers(h-> h.frameOptions( fo -> fo.disable()))
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers( "/h2-console/**").permitAll()) // /api/**
+               // .authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers( "/api/products/**").hasAuthority("ADMIN"))
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
+                // define jwtAuthConverter class
+                .oauth2ResourceServer(o2-> o2.jwt(jwt-> jwt.jwtAuthenticationConverter(jwtAuthConverter))) //securiter oauth2 à chaque requet cherche jwt dans header et verifier la signature à la fin charger le contexte de securiter roles
+
+                .build() ;
+    }
+
+    @Bean // cors configuation
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("*"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+
+
+}
