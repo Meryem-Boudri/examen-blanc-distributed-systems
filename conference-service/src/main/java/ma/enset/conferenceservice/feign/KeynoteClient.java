89dd552186f0b2d695b2f50500596558b2ce5db0
meryem-boudri <meryem-boudri@github.com>
2024-12-22 17:39:42 +0100

    fc

--- conference-service/src/main/java/ma/enset/conferenceservice/feign/KeynoteClient.java
+++ conference-service/src/main/java/ma/enset/conferenceservice/feign/KeynoteClient.java
@@ -1,12 +1,13 @@
 package ma.enset.conferenceservice.feign;
 
+import ma.enset.conferenceservice.interceptors.FeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "keynote-service", url = "http://localhost:8083")
+@FeignClient(name = "keynote-service", url = "http://localhost:8083", configuration = FeignConfig.class)
 
 
 public interface KeynoteClient {
